fe00fafaa0e728cdc911d1d29d277751c45051d6
Perly Gustafsson <contact@daperley.com>
2017-09-27 11:14:27 +0200

    Recourses exercise

---
+++ ExerciseOneTwentySeventh/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using ExerciseOneTwentySeventh.Models;
+using System.Windows;
+
+namespace ExerciseOneTwentySeventh
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = Product.GetProducts();
+        }
+    }
+}
---
+++ ExerciseOneTwentySeventh/Models/Product.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace ExerciseOneTwentySeventh.Models
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+
+        public static List<Product> GetProducts()
+        {
+            var products = new List<Product>() {
+                new Product()
+                {
+                    Id=1,
+                    Title ="Oligio Mint Choclate Chip",
+                    Price=15
+                },
+                new Product()
+                {
+                    Id=1,
+                    Title ="Oligio Cookies and Cream",
+                    Price=15
+                },
+                new Product()
+                {
+                    Id=1,
+                    Title ="Oligio Banana Caramell",
+                    Price=15
+                }
+            };
+
+            return products;
+        }
+    }
+}
