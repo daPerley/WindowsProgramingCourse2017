b373c96f9f8ab0a91777a68b93bf4b29f04dd5bc
Perly Gustafsson <contact@daperley.com>
2017-09-27 14:05:58 +0200

    Timer working

--- ExerciseTwoTwentySeventh/MainWindow.xaml.cs
+++ ExerciseTwoTwentySeventh/MainWindow.xaml.cs
@@ -1,28 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ExerciseTwoTwentySeventh
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer timer;
+        private TimeSpan increment;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            TimerLabel.DataContext = "00:00:00";
+
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 1);
+            timer.Tick += Thicker;
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(e.Uri.AbsoluteUri);
+        }
+
+        private void Thicker(object sender, EventArgs e)
+        {
+            increment = increment.Add(timer.Interval);
+            TimerLabel.DataContext = increment;
+        }
+
+        private void BtnStart_Timer(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void BtnStop_Timer(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void BtnReset_Timer(object sender, RoutedEventArgs e)
+        {
+            increment = new TimeSpan();
+            TimerLabel.DataContext = increment;
         }
     }
 }
